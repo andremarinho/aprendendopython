be9e5ebacaf708163c0526c4d6b05d8deabc70fb
andremarinho1 <andre.bmarinho@hotmail.com>
2020-01-10 06:10:20 -0300

    Conclusão do desagio023

--- desafio23.py
+++ desafio23.py
@@ -1,1 +1,13 @@
 print('='*10,' DESAFIO 23','='*10)
+print("""Programa que leia um numero de 0 a 9999'
+e mostre na cada um dos digitos separados \n""")
+
+num = int(input('Informe um numero: '))
+print('Analizando o numero {} '.format(num))
+
+
+
+print('A unidade é {} '.format(num // 1 % 10))
+print('A dezena é {} '.format(num // 10 % 10))
+print('A centena é {} '.format(num // 100 % 10))
+print('A milhar é {} '.format(num // 1000 % 10))

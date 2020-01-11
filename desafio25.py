67b755cd332dad83731fde3f72c0cec025b8c3f7
andremarinho1 <andre.bmarinho@hotmail.com>
2020-01-11 05:55:04 -0300

    Desafio 25 concluido

---
+++ desafio25.py
@@ -0,0 +1,4 @@
+print('='*10,' DESAFIO 25 ','='*10)
+nome = str(input('Digite seu nome completo: ').strip())
+print('Seu nome comtém a palavra Silva? Analisamos e o resultado foi: ')
+print('SILVA' in nome.upper())

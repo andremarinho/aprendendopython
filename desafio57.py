7219e316742bcd3e59bda1c7dda272ab2e06699d
andremarinho <andre.bmarinho@hotmail.com>
2020-02-16 07:18:53 -0300

    Desafio 57 - Algoritmo que pede informação sobre sexo e só aceita dois valores possiveis M ou F

---
+++ desafio57.py
@@ -0,0 +1,12 @@
+print('='*10,' DESAFIO 57 ','='*10)
+
+
+while True:
+
+    sexo  = input('Digite F para feminino e M para masculino: ')
+    sexo.upper()
+
+
+    while sexo != "M" and sexo != "F":
+
+        sexo = input('O sistema só aceita M e F, por favor tente novamente: ')

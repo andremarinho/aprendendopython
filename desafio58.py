96c0ca5e29fbf30acd3483c6e1ec74377af162a6
andremarinho <andre.bmarinho@hotmail.com>
2020-02-19 05:11:51 -0300

    Desafio 58 - melhoramento do jogo

--- desafio58.py
+++ desafio58.py
@@ -9,13 +9,15 @@ print("""
 ESTE PROGRAMA VAI FAZER O COMPUTADOR 'PENSAR' EM UM NUMERO
 INTEIRO E PEDIR PARA O USUARIO ADVINHAR
 """)
-numeroComputador = randint(0,5) # Faz o computador sortear um numero
-print('PROCESSANDO estou pensando em numero...')
+numeroComputador = randint(0,10) # Faz o computador sortear um numero
+print('PROCESSANDO estou pensando em numero de 0 a 10...')
 sleep(2)
 numeroUsuario = int(input('Aposte em um numero: '))
+tentativas_usuario = 0;
 
 while numeroComputador != numeroUsuario:
     print('Você errou, tente novamente')
+    tentativas_usuario = tentativas_usuario + 1
     numeroUsuario = int(input('Digite novo numero: '))
 
-print('Você ganhou!!!' if numeroComputador == numeroUsuario else 'Você perdeu',' numero sorteador foi {}'.format(numeroComputador))
+print('Você ganhou!!! o numero sorteado foi {} e você precisou de {} tentaivas para ganhar o jogo.'.format(numeroComputador, tentativas_usuario))
